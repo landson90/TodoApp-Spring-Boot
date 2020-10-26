2b24c57b2613b8376cad80e74980a7b25bcd7131
Landson <randelsilva1990@gmail.com>
2020-10-26 17:08:20 -0300

    criando a model task

---
+++ src/main/java/com/seventec/TodoApp/models/TaskEntity.java
@@ -0,0 +1,84 @@
+package com.seventec.TodoApp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tarefas")
+public class TaskEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Boolean status;
+	
+	public TaskEntity() { }
+	
+	public TaskEntity(Long id, String title, Boolean status) {
+		this.id = id;
+		this.title = title;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskEntity other = (TaskEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TaskEntity [id=" + id + ", title=" + title + ", status=" + status + "]";
+	}
+	
+	
+	
+	
+}
