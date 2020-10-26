7607e4775a9686035fdad5753aa92b109e459229
Landson <randelsilva1990@gmail.com>
2020-10-26 17:43:17 -0300

    criando o metodo salvar e listar

---
+++ src/main/java/com/seventec/TodoApp/controller/TaskController.java
@@ -0,0 +1,34 @@
+package com.seventec.TodoApp.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seventec.TodoApp.dto.TaskDto;
+import com.seventec.TodoApp.service.TaskService;
+
+@RestController
+@RequestMapping(value = "/tarefas")
+public class TaskController {
+
+	private TaskService taskService;
+	
+	public TaskController(TaskService taskService) {
+		this.taskService = taskService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TaskDto>> index() {
+		return this.taskService.index();
+	}
+	
+	@PostMapping
+	public ResponseEntity<TaskDto> store(@RequestBody TaskDto taskDto) {
+		return this.taskService.store(taskDto);
+	}
+}
--- src/main/java/com/seventec/TodoApp/service/TaskService.java
+++ src/main/java/com/seventec/TodoApp/service/TaskService.java
@@ -1,9 +1,18 @@
 package com.seventec.TodoApp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.seventec.TodoApp.dto.TaskDto;
+import com.seventec.TodoApp.models.TaskEntity;
 import com.seventec.TodoApp.repository.TaskRepository;
+
+import java.net.URI;
 
 @Service
 public class TaskService {
@@ -13,6 +22,24 @@ public class TaskService {
 	@Autowired
 	public TaskService(TaskRepository taskRepository) {
 		this.taskRepository = taskRepository;
+	}
+
+	public ResponseEntity<List<TaskDto>> index() {
+		List<TaskEntity> tasksEntity = this.taskRepository.findAll();
+		List<TaskDto> tasksDto = tasksEntity.stream().map(t -> new TaskDto(t)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(tasksDto);
+	}
+
+	public ResponseEntity<TaskDto> store(TaskDto taskDto) {
+		TaskEntity taskEntity = this.taskRepository.save(this.convertTaskDtoInTaskEntity(taskDto));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(taskEntity.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TaskDto(taskEntity));
+	}
+
+	private TaskEntity convertTaskDtoInTaskEntity(TaskDto taskDto) {
+		return new TaskEntity(taskDto.getId(), taskDto.getTitle(), taskDto.getStatus());
 	}
 
 }
